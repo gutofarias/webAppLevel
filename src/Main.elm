c9edf137677c4638874157d05bf0b9809001318e
Joao Gutemberg Farias <joao.gutemberg.farias@gmail.com>
2021-08-18 11:48:38 -0300

    Adicionando o teste da animação nos gráficos e com a figura

--- src/Main.elm
+++ src/Main.elm
@@ -7,7 +7,8 @@ import Controller as Control
 import Reference as Ref
 
 import Browser
-import Html exposing (Html, button, div, text, pre, input, label, select, option, span)
+import Browser.Events
+import Html exposing (Html, button, div, text, pre, input, label, select, option, span, section)
 import Html.Attributes exposing (style, placeholder, value, for, name, selected)
 import Html.Events exposing (onClick, onInput)
 
@@ -25,7 +26,7 @@ import MyChart as MC
 ------------------------------------------------
 
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+  Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
       
       
@@ -46,6 +47,8 @@ type alias Model =
     , chartsParam : List MC.ChartParam 
     , controlParam : Control.ControlParam
     , refParam : Ref.RefParam
+    , tfimAnimation : Float
+    , animating : Bool
     }
 
     
@@ -62,7 +65,7 @@ type alias InteractStates =
     { edoIStates : Edo.EdoIStates
     , modelIStates : M.ModelIStates
     -- , chartIStates : MC.ChartIStates
-    , controlIStates : Control.ControlIStates
+    -- , controlIStates : Control.ControlIStates
     , refIStates : Ref.RefIStates
     }
 
@@ -79,8 +82,8 @@ updateModelIStates modelIStates interactStates =
 -- init
 ------------------------------------------------
 
-init : Model 
-init =
+init : () -> (Model, Cmd Msg) 
+init () =
     let
         ag = 1.0
         ap = 0.1
@@ -96,22 +99,33 @@ init =
         chartData = []
         levelIStates = {h0 = (String.fromFloat h0), ag =  (String.fromFloat ag), ap = (String.fromFloat ap)}
         edoIStates = {tini = (String.fromFloat tini), tfim = (String.fromFloat tfim)}
-        controlIStates = Control.PidIS Control.initPidIStates
+        -- controlIStates = Control.PidIS Control.initPidIStates
         refIStates = Ref.Step1IS Ref.initStep1IStates
-        interactStates = {edoIStates = edoIStates, modelIStates = M.LevelIS levelIStates, controlIStates = controlIStates, refIStates = refIStates}
+        interactStates = {edoIStates = edoIStates, modelIStates = M.LevelIS levelIStates, refIStates = refIStates}
     in
-            { chartData = chartData
+           ({ chartData = chartData
             , modelParam = M.LevelP levelParam
             , edoParam = edoParam
             , interactStates = interactStates
             -- , str = "teste"
+            , tfimAnimation = 0.0
+            , animating = False
             , chartsParam = [(MC.initChartParam Nothing)]
             , controlParam = Control.PidP Control.initPidParam
             , refParam = Ref.Step1P Ref.initStep1Param
-            }
+            }, Cmd.none)
 
         
-        
+------------------------------------------------
+-- Subscriptions
+------------------------------------------------
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    case model.animating of
+        False ->
+            Sub.none
+        True ->
+            Browser.Events.onAnimationFrameDelta (Tick)
         
         
 ------------------------------------------------
@@ -123,32 +137,39 @@ type Msg
     | ChangeInteract Interact
     | UpdateParameters 
     -- | ChangeStr String
+    | Tick Float
+    | RunAnimation
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
       
     RunEdo -> 
       let  
-          newModel = update UpdateParameters model
+          animating = False
+          newModel = Tuple.first <| update UpdateParameters model
           edoParam = .edoParam newModel 
           modelParam = .modelParam newModel
           controlParam = .controlParam newModel
           refParam = .refParam newModel
+                     
           controller = Control.controllerFromControlParam controlParam
           refFunc = Ref.refFromRefParam refParam 
           refFuncAndController = {refFunc = refFunc,controller = controller}
           maybeRefFuncAndController = Just refFuncAndController
-          data = M.runEdoModel modelParam edoParam maybeRefFuncAndController
+                                      
+          controlMem = []
+          newEdoParam = {edoParam | controlMemory = controlMem}
+                                      
+          data = M.runEdoModel modelParam newEdoParam maybeRefFuncAndController
       in
-          { newModel | chartData = data }
+          ({ newModel | chartData = data, animating = animating, edoParam = newEdoParam }, Cmd.none)
             
     ChangeInteract interact ->
         let
            interactStates = .interactStates model
            edoIStates = .edoIStates interactStates
            modelIStates = .modelIStates interactStates
-           controlIStates = .controlIStates interactStates
         in
             case interact of
                 Edo edoInteract ->
@@ -156,22 +177,21 @@ update msg model =
                         edoIStatesNew = Edo.changeEdoIStates edoIStates edoInteract 
                         interactStatesNew = {interactStates | edoIStates = edoIStatesNew}
                     in
-                        {model | interactStates = interactStatesNew}
+                        ({model | interactStates = interactStatesNew}, Cmd.none)
                         
                 Models modelInteract ->
                     let
                         modelIStatesNew = M.changeModelIStates modelIStates modelInteract
                         interactStatesNew = {interactStates | modelIStates = modelIStatesNew}
                     in 
-                        {model | interactStates = interactStatesNew}
+                        ({model | interactStates = interactStatesNew}, Cmd.none)
                             
                 Control controlInteract ->
                     let 
-                        controlIStatesNew =
-                            Control.changeControlIStates controlIStates controlInteract 
-                        interactStatesNew = {interactStates | controlIStates = controlIStatesNew}
+                        controlParam = .controlParam model
+                        controlParamNew = Control.changeControlParam controlParam controlInteract
                     in
-                        {model | interactStates = interactStatesNew}
+                        ({model | controlParam = controlParamNew}, Cmd.none)
 
                 Ref refInteract ->
                     let
@@ -179,21 +199,21 @@ update msg model =
                         refIStatesNew = Ref.changeRefIStates refIStates refInteract 
                         interactStatesNew = {interactStates | refIStates = refIStatesNew}
                     in
-                        {model | interactStates = interactStatesNew}
+                        ({model | interactStates = interactStatesNew}, Cmd.none)
 
                 MChart chartID chartInteract -> 
                     let
                         chartsParam = .chartsParam model
                         newChartsParam = MC.chartIndividualInteractAction chartID chartsParam chartInteract
                     in
-                        {model | chartsParam = newChartsParam}
+                        ({model | chartsParam = newChartsParam}, Cmd.none)
 
                 MCharts chartsInteract ->
                     let
                         chartsParam = .chartsParam model
                         newChartsParam = MC.chartsInteractAction chartsParam chartsInteract
                     in
-                        {model | chartsParam = newChartsParam}
+                        ({model | chartsParam = newChartsParam}, Cmd.none)
 
 
     UpdateParameters ->
@@ -201,7 +221,7 @@ update msg model =
             interactStates = .interactStates model
             edoIStates = .edoIStates interactStates
             modelIStates = .modelIStates interactStates
-            controlIStates = .controlIStates interactStates
+            -- controlIStates = .controlIStates interactStates
             refIStates = .refIStates interactStates
                          
             edoParam = .edoParam model
@@ -211,10 +231,72 @@ update msg model =
                        
             edoParamNew = Edo.updateEdoParam edoParam edoIStates
             modelParamNew = M.updateModelParam modelParam modelIStates
-            controlParamNew = Control.updateControlParam controlParam controlIStates
+            -- controlParamNew = Control.updateControlParam controlParam controlIStates
             refParamNew = Ref.updateRefParam refParam refIStates
         in
-            {model | edoParam = edoParamNew, modelParam = modelParamNew, controlParam = controlParamNew, refParam = refParamNew}
+            ({model | edoParam = edoParamNew, modelParam = modelParamNew, refParam = refParamNew}, Cmd.none)
+
+    Tick dTime ->
+        let
+            animating = .animating model
+        in
+        case animating of
+            False -> 
+                (model, Cmd.none)
+            True -> 
+                let
+                    dTimeSec = dTime/1000.0
+                    edoParam = .edoParam model
+                    tfimAnimation = .tfimAnimation model
+                    tempo = .tempo edoParam
+                    tfim = .tfim edoParam
+                    newTfim = if (tfim + dTimeSec) >= tfimAnimation then
+                                  tfimAnimation
+                              else
+                                  tfim + dTimeSec
+                    edoParam2 = {edoParam | tfim = newTfim}
+                in
+                if (tempo >= tfimAnimation) then
+                    let
+                        newAnimating = False
+                    in
+                    ({model | animating=newAnimating}, Cmd.none)
+                else
+                let
+                    modelData = .chartData model
+                    modelParam = .modelParam model
+                    controlParam = .controlParam model
+                    refParam = .refParam model
+                    controller = Control.controllerFromControlParam controlParam
+                    refFunc = Ref.refFromRefParam refParam 
+                    refFuncAndController = {refFunc = refFunc,controller = controller}
+                    maybeRefFuncAndController = Just refFuncAndController
+                    (data,newEdoParam) = M.runAnimationModel modelParam edoParam2 maybeRefFuncAndController
+                    dataFirst = Maybe.withDefault (DC.TS1 {t=5.0, x1=50.0}) <| List.head data 
+                    xs = DC.xsFromDatum dataFirst
+                    -- xs2 = Debug.log "teste" xs
+                    -- xs = [1.0]
+                    modelParamNew = M.initStateModelFromXs xs modelParam
+                    newData = data ++ modelData
+                in
+                -- ({model | elapsedTime = newElapsedTime}, Cmd.none)
+                ({ model | chartData = newData, edoParam = newEdoParam
+                 , modelParam = modelParamNew
+                 }, Cmd.none)
+
+    RunAnimation ->
+        let
+            animating = True
+            newModel = Tuple.first <| update UpdateParameters model
+            edoParam = .edoParam newModel
+            controlMem = []
+
+            tfim = .tfim edoParam
+            newEdoParam = {edoParam | tfim = 0.0, controlMemory = controlMem}
+            chartData = []
+            
+        in
+           ({newModel | animating = animating, tfimAnimation = tfim, edoParam = newEdoParam, chartData = chartData }, Cmd.none)
 
     -- ChangeStr str -> 
     --      {model | str = str}
@@ -228,28 +310,51 @@ update msg model =
 view : Model -> Html Msg
 view model =
   let
+    controlParam = .controlParam model
     interactStates = .interactStates model
     edoIStates = .edoIStates interactStates
     modelIStates = .modelIStates interactStates
-    controlIStates = .controlIStates interactStates
+    -- controlIStates = .controlIStates interactStates
     refIStates = .refIStates interactStates
     chartData = .chartData model
     chartsParam = .chartsParam model
+    edoParam = .edoParam model
+    refParamI = .refParam model
+    refParam = case refParamI of
+                   Ref.Step1P stepParam -> stepParam
+    refmax = max (.iVal refParam) (.fVal refParam)
+    modelParam = .modelParam model
+    levelParam = case modelParam of
+                     M.LevelP levelParam1 -> levelParam1
+         
+    h0 = 10.0
+    hmaxExpected = max h0 refmax 
+    data = .chartData model
+    dataFirst = Maybe.withDefault (DC.TS1 {t=0.0, x1=10.0}) <| List.head data 
+    xs = DC.xsFromDatum dataFirst
+    us = DC.usFromDatum dataFirst
+    level = Maybe.withDefault 10.0 <| List.head xs
+    u = Maybe.withDefault 10.0 <| List.head us
+    -- levellog = Debug.log "level" (level,hmaxExpected)
   in
-    div []
-        [
-        div []
-            (
-            [ Edo.viewEdoIStates edoIStates (ChangeInteract << Edo)
-            , M.viewModelIStates modelIStates (ChangeInteract << Models)
-            , Control.viewControlIStates controlIStates (ChangeInteract << Control)
-            , div [] [Ref.viewRefIStates refIStates (ChangeInteract << Ref)]
-            , div [] []
-            , button [ onClick RunEdo ] [ text "Edo" ]
-            -- , label [] [ text model.str]
+    section []
+        [ div [style "height" "30px"]
+            [ Edo.viewEdoIStates edoIStates (ChangeInteract << Edo)]
+        , div [style "height" "30px"]
+            [ M.viewModelIStates modelIStates (ChangeInteract << Models)]
+        , div [style "height" "30px"]
+            [Control.viewController controlParam (ChangeInteract << Control)]
+        , div [style "height" "30px"]
+            [Ref.viewRefIStates refIStates (ChangeInteract << Ref)]
+        , div [style "height" "30px"]
+            [ button [ onClick RunEdo ] [ text "Edo" ]
+            , button [ onClick RunAnimation ] [ text "Animation" ]
+            -- , text (String.fromFloat <| .elapsedTime model)
+            , text (String.fromFloat <| .tempo edoParam)
             ]
-            ++ MC.chartsView chartData chartsParam (ChangeInteract << MCharts) (fcomposition23 ChangeInteract MChart)
-            )
+        , span []
+            (MC.chartsView chartData chartsParam (ChangeInteract << MCharts) (fcomposition23 ChangeInteract MChart))
+        , M.levelSim level u 0.0 hmaxExpected levelParam
         -- , MC.chartView chartData chartParam (fcomposition23 ChangeInteract MChart)
         ]
          
--- src/Models.elm
+++ src/Models.elm
@@ -6,6 +6,15 @@ import Html exposing (Html,div,text,input,span,pre,label)
 import Html.Attributes exposing (style, placeholder, value, type_)
 import Controller
 import Html.Events exposing (onInput)
+import Drawing2d
+import Pixels exposing (pixels)
+import Point2d
+import Rectangle2d
+import Polygon2d
+import Color
+import Angle
+import Arc2d
+import Polyline2d
 
 ------------------------------------------------
 ------------------------------------------------
@@ -29,6 +38,11 @@ updateModelParam modelParam modelIStates =
                 LevelIS levelIStates ->
                    LevelP <| updateLevelParam levelParam levelIStates
 
+initStateModelFromXs : Edo.State -> ModelParam -> ModelParam
+initStateModelFromXs xs modelParam =
+    case modelParam of
+        LevelP levelParam ->
+            LevelP <| initStateLevelFromXs xs levelParam
                        
 ------------------------------------------------
 -- ModelIStates
@@ -85,6 +99,11 @@ runEdoModel modelParam edoParam maybeRefAndController =
              runEdoLevel levelParam edoParam maybeRefAndController
                  
                  
+runAnimationModel : ModelParam -> Edo.EdoParam -> Maybe {refFunc: Edo.RefFunction, controller:Edo.Controller} -> (DC.ChartData, Edo.EdoParam)
+runAnimationModel modelParam edoParam maybeRefAndController =
+     case modelParam of
+         LevelP levelParam ->
+             runAnimationLevel levelParam edoParam maybeRefAndController
                  
                  
                  
@@ -123,6 +142,15 @@ updateLevelParam levelParam levelIStates =
                    {levelParam | initState = initState, geoParam = geoParam}
             _ -> 
                 levelParam
+                    
+initStateLevelFromXs : Edo.State -> LevelParam -> LevelParam
+initStateLevelFromXs xs levelParam = 
+    let
+        initState = .initState levelParam
+        newInitStateVal = Maybe.withDefault 30.0 <| List.head xs
+        newInitState = {h0 = newInitStateVal}
+    in
+        {levelParam | initState = newInitState}
                     
 ------------------------------------------------
 -- LevelIStates
@@ -184,6 +212,48 @@ runEdoLevel levelParam edoParam maybeRefFuncAndController =
             in
                 runEdoLevelControlled levelParam edoParam refFunc controller
                 
+runAnimationLevel : LevelParam -> Edo.EdoParam -> Maybe {refFunc: Edo.RefFunction, controller:Edo.Controller} -> (DC.ChartData, Edo.EdoParam)
+runAnimationLevel levelParam edoParam maybeRefFuncAndController =
+    case maybeRefFuncAndController of
+        Nothing ->
+             runAnimationLevelUncontrolled levelParam edoParam
+
+        Just refFuncAndController ->
+            let
+                refFunc = .refFunc refFuncAndController
+                controller = .controller refFuncAndController
+            in
+                runAnimationLevelControlled levelParam edoParam refFunc controller
+             -- runAnimationLevelUncontrolled levelParam edoParam
+                
+runAnimationLevelUncontrolled : LevelParam -> Edo.EdoParam -> (DC.ChartData, Edo.EdoParam)
+runAnimationLevelUncontrolled levelParam edoParam =
+        let
+            initState = ( .h0 <| .initState levelParam ) :: []
+            geoParam = .geoParam levelParam
+            edoSist = Edo.Uncontrolled (levelSyst levelParam [0.0])
+            (edoData, edoParamNew) = Edo.edoSolverReversed edoParam edoSist initState
+        in
+            -- DC.toChartDataTS1E1R1U1 <| Tuple.first <| Edo.edoSolver edoParam edoSist initState
+            (DC.toChartDataTS1 edoData, edoParamNew)
+                    
+                
+runAnimationLevelControlled : LevelParam -> Edo.EdoParam -> Edo.RefFunction  -> Edo.Controller -> (DC.ChartData, Edo.EdoParam)
+runAnimationLevelControlled levelParam edoParam refFunc controller =
+        let
+            initState = ( .h0 <| .initState levelParam ) :: []
+            geoParam = .geoParam levelParam
+            -- edoParam2 = Debug.log "edoParam" edoParam
+            edoSist = Edo.Controlled
+                      { refFunc = refFunc
+                      , outputFunc = outputX1
+                      , controller = controller
+                      , sistFunc = (levelSyst levelParam)}
+                
+            (edoData, edoParamNew) = Edo.edoSolverReversed edoParam edoSist initState
+        in
+            (DC.toChartDataTS1E1R1U4 edoData, edoParamNew)
+                
 runEdoLevelControlled : LevelParam -> Edo.EdoParam -> Edo.RefFunction  -> Edo.Controller -> DC.ChartData
 runEdoLevelControlled levelParam edoParam refFunc controller =
         let
@@ -195,7 +265,7 @@ runEdoLevelControlled levelParam edoParam refFunc controller =
                       , controller = controller
                       , sistFunc = (levelSyst levelParam)}
         in
-            DC.toChartDataTS1E1R1U1 <| Tuple.first <| Edo.edoSolver edoParam edoSist initState
+            DC.toChartDataTS1E1R1U4 <| Tuple.first <| Edo.edoSolver edoParam edoSist initState
             -- DC.toChartDataTS1 <| Tuple.first <| Edo.edoSolver edoParam edoSist initState
 
 runEdoLevelUncontrolled : LevelParam -> Edo.EdoParam -> DC.ChartData
@@ -222,14 +292,10 @@ levelSyst param us t state =
        u = if (uAux<0.0) then 0.0 else uAux
     in
        case state of
-           h::[] ->
-               let
-                   hn = if h >= 0.0 then h else 0.0
-               in 
-                   -(ap/ag)*sqrt(2.0*g)*sqrt(hn) + (u/ag) :: []
            h::ls -> 
                let
                    hn = if h >= 0.0 then h else 0.0
+                   -- hnlog = Debug.log "sist" (hn,u)
                in 
                    -(ap/ag)*sqrt(2.0*g)*sqrt(hn) + (u/ag) :: []
            _ -> 0.0 :: []
@@ -241,3 +307,133 @@ outputX1 tempo xs =
     case xs of
         (x::ls) -> [x]
         _ -> xs
+
+
+levelSim : Float -> Float -> Float -> Float -> LevelParam -> Html msg
+levelSim level input ref hmaxExpected levelParam = 
+    let
+        viewBox =
+            Rectangle2d.from Point2d.origin (Point2d.pixels 800 450)
+        
+        geoParam = .geoParam levelParam
+        ag = .ag geoParam
+        ap = .ap geoParam
+        lbase = 200.0
+        l = lbase*sqrt(ag)
+        hbase = 300.0
+        h = hbase
+        e = 10.0    
+        esc = 30.0
+        abbase = 20.0
+        ab = 3*abbase*sqrt(ap)
+        xini = 100.0
+        yini = 100.0
+
+        pa = Point2d.pixels xini yini
+        pb = Point2d.pixels (xini+l) yini
+        pc = Point2d.pixels xini (yini+h)
+        pd = Point2d.pixels (xini+l) (yini+h)
+             
+        p1 = extendP2d pb (esc+e) 0.0
+        p2 = extendP2d p1 0.0 (-e)
+        p3 = extendP2d pa (-e) (-e)
+        p4 = extendP2d pc (-e) (-e-ab)
+        p5 = extendP2d p4 (-esc) (0.0)
+        p6 = extendP2d p5 0.0 e
+        p7 = extendP2d p6 (e+esc) 0.0
+
+        p8 = extendP2d pc (-e-esc) 0.0
+        p9 = extendP2d p8 0.0 e
+        p10 = extendP2d pd e e
+        p11 = extendP2d pb e (ab+e)
+        p12 = extendP2d p11 (esc) 0.0
+        p13 = extendP2d p12 0.0 (-e)
+        p14 = extendP2d pb 0.0 (ab)
+
+                 
+        poly1 = Polygon2d.singleLoop [pa,p1,p2,p3,p4,p5,p6,p7]
+        poly2 = Polygon2d.singleLoop [pc,p8,p9,p10,p11,p12,p13,p14,pd]
+                
+        levelmax = 1.5*hmaxExpected
+        levelPixel = (level/levelmax)*h
+        prect1 = pa
+        prect2 = extendP2d prect1 l levelPixel
+        rect = Rectangle2d.from prect1 prect2
+
+        hrb = Maybe.withDefault 0.0 <| List.minimum [levelPixel,ab]
+        prb1 = pb 
+        prb2 = extendP2d prb1 (e+esc) hrb
+        rectb = Rectangle2d.from prb1 prb2
+        
+        input2 = max input 0.0
+        -- qo = input2*10.0
+        qo = input2/ap
+        -- qo = 10.0
+        hqo = if (qo > ab) then ab
+              else if (qo < 0.0) then 0.0
+              else qo
+                  
+        pra11 = p6
+        pra12 = extendP2d pra11 (esc+e) (hqo)
+        recta1 = Rectangle2d.from pra11 pra12
+
+        pra21 = extendP2d pra11 (esc+e+hqo) 0.0
+        pra22 = pa
+        recta2 = Rectangle2d.from pra21 pra22
+
+        parca1 = pra12
+        -- parca1 = Point2d.pixels 200.0 200.0
+        parca2 = pra21
+        -- parca2 = Point2d.pixels 250.0 250.0
+        anglea = Angle.degrees 90.0
+        arca = Arc2d.from parca2 parca1 anglea
+
+        nSegs = 15
+        arcAsPolylineSegsA = Arc2d.segments nSegs arca
+        polya = Polygon2d.singleLoop ((Arc2d.centerPoint arca)::(Polyline2d.vertices arcAsPolylineSegsA))
+        
+        waterFall = 50.0
+        prc1 = p1
+        prc2 = extendP2d prc1 (hrb) (-waterFall)
+        rectc = Rectangle2d.from prc1 prc2
+                
+        parcb1 = extendP2d p1 0.0 hrb
+        parcb2 = extendP2d p1 hrb 0.0
+        angleb = Angle.degrees 90.0
+        arcb = Arc2d.from parcb2 parcb1 angleb
+               
+        arcAsPolylineSegsB = Arc2d.segments nSegs arcb
+        polyb = Polygon2d.singleLoop ((Arc2d.centerPoint arcb)::(Polyline2d.vertices arcAsPolylineSegsB))
+                
+        points = [pa,pb,pc,pd,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,parca1,parca2,prc1,prc2,parcb1,parcb2]
+    in 
+    Drawing2d.draw
+        { viewBox = viewBox
+        , entities = [
+              Drawing2d.group [Drawing2d.fillColor Color.lightBlue, Drawing2d.strokeWidth <| Pixels.float 0.0]
+                [ Drawing2d.rectangle [] rect
+                , Drawing2d.rectangle [] rectb
+                , Drawing2d.rectangle [] recta1
+                , Drawing2d.rectangle [] recta2
+                , Drawing2d.rectangle [] rectc
+                -- , Drawing2d.arc [Drawing2d.strokeWidth <| Pixels.float 1.0] arca
+                , Drawing2d.polygon [] polya
+                -- , Drawing2d.arc [Drawing2d.strokeWidth <| Pixels.float 1.0] arcb
+                , Drawing2d.polygon [] polyb
+                ]
+            , Drawing2d.group [Drawing2d.fillColor Color.grey]
+                [ Drawing2d.polygon [] poly1        
+                , Drawing2d.polygon [] poly2
+                ]
+            ] 
+            -- ++ List.map Common.dot points
+        }
+
+        
+extendP2d : Point2d.Point2d Pixels.Pixels coordinates -> Float -> Float -> Point2d.Point2d Pixels.Pixels coordinates
+extendP2d p dx dy =
+   let 
+       px = Pixels.toFloat (Point2d.xCoordinate p)
+       py = Pixels.toFloat (Point2d.yCoordinate p)
+   in
+       Point2d.pixels (px + dx) (py + dy)
